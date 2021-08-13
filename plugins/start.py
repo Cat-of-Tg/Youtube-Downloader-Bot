55eea41a73673cb1dba00bf52f44a142019bc9f5
PoisonBot <86126808+PoisonBot@users.noreply.github.com>
2021-08-13 21:04:35 +0530

    start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,9 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Support Group", url="https://t.me/Mercy_bots")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/Mercy_Bots")],
+       [InlineKeyboardButton("🍭Dev", url="https://t.me/ok_bie_bot)]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
